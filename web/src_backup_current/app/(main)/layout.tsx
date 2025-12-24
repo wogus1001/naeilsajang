d5b1ec84e184bc3c77fc874140a1c4daf426fec3
jaehyun <wogus1001@gmail.com>
2025-12-24 14:35:30 +0900

    Chore: Successfully moved 'web' folder into ERP folder

--- web/src_backup_current/app/(main)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import MainLayout from "@/components/layout/MainLayout";
-
-export default function Layout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    return <MainLayout>{children}</MainLayout>;
-}
